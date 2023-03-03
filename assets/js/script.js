221a9bc2e6f337381b3e57e97b27324801fbf42a
ThMenezes <thita.menezes24@gmail.com>
2023-03-02 21:43:36 -0300

    adicionando função do segundo botão

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,34 @@ function adicionar() {
     numero.value = '' // limpar após incluir número
     numero.focus()   // deixar piscando no input
 }
+
+function finalizar() {
+
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar')
+        
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let posição in valores) { 
+            soma += valores[posição]
+            if (valores[posição] > maior)
+            maior = valores[posição]
+            if (valores[posição] < menor)
+            menor = valores[posição]
+        }
+
+        media = soma / total
+
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p>Ao todo temos ${total} números cadastrados!</p>`
+        resultado.innerHTML += `<p>O <u>maior</u> número encontrado é ${maior}.</p>`
+        resultado.innerHTML += `<p>O <u>menor</u> número encontrado é ${menor}.<p>`
+        resultado.innerHTML += `<p>A <u>soma</u> dos números é ${soma}.<p>`
+        resultado.innerHTML += `<p>A <u>média</u> dos valores é ${media}.<p>`
+    }
+}
